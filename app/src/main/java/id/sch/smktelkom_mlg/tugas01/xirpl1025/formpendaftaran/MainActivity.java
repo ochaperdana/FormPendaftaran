1016b4de265015d0d73b04c3bebaa245755adf3e
Ocha Putri Perdana Prihatina <ocha_prihatina_24rpl@student.smktelkom-mlg.sch.id>
2016-09-10 21:46:55 +0700

    (+)first commit : starting project (+)second commit : making new design (+)third commit : initializing

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1025/formpendaftaran/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1025/formpendaftaran/MainActivity.java
@@ -2,8 +2,20 @@ package id.sch.smktelkom_mlg.tugas01.xirpl1025.formpendaftaran;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama, etTempat, etTanggal;
+    TextView tvHasil;
+    Button buttonRegister;
+    RadioGroup rgJK;
+    CheckBox cbTOEFL, cbTOEIC, cbReg, cbIntr;
+    Spinner spKelas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
